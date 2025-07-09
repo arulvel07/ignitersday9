f4c32dbabb87533e68e69605a774d251d57da6d2
Arul <cs24i1027@iiitdm.ac.in>
2025-07-10 01:53:58 +0530

    question 1 completed

---
+++ qn1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        int n, q;
+        scanf("%d %d", &n, &q);
+        int a[n];
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &a[i]); //take input for array
+        }
+        for (int i = 0; i < q; i++) {
+            int x;
+            scanf("%d", &x);    //take input for enchantment
+            int power = (int)pow(2, x);       // 2^x
+            int add = (int)pow(2, x - 1);     // 2^(x - 1)
+            for (int j = 0; j < n; j++) {
+                if (a[j] % power == 0) {      //add if divisible by power
+                    a[j] += add;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) { //print final
+            printf("%d ", a[i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
